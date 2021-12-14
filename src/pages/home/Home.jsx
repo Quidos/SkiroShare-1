7d355d82c13095cb5e898079ff3071eea4380a8b
Denis Fera <denis.feratovic10@gmail.com>
2021-12-14 19:35:38 +0100

    zacel zacetno stran

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import ResponsiveDrawer from "../../components/drawer/Drawer";
+
+const Home = () => {
+  return (
+    <div>
+      <ResponsiveDrawer />
+    </div>
+  );
+};
+
+export default Home;
