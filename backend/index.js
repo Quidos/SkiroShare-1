f8828a31994aef768e1c6970ef174bb44c2e998a
Denis Fera <denis.feratovic10@gmail.com>
2022-01-04 19:08:06 +0100

    'fixes'

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const pool = new pg.Pool({
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
